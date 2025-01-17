7ab5a8fc43bf946cb0fd669ff282efa195f78d16
LautaroMalleret <122919507+LautaroMalleret@users.noreply.github.com>
2025-01-17 15:02:21 -0300

    Update Controller.java

--- src/main/java/com/app/CANCHASBA_API/controller/Controller.java
+++ src/main/java/com/app/CANCHASBA_API/controller/Controller.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:4200") // Cambia según la URL de tu frontend
 @RestController
 @RequestMapping("/api/v1")
 public class Controller {
